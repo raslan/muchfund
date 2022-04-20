e249148cf366ace522bd17609efd720ace9421fb
raslan <24810123+raslan@users.noreply.github.com>
2022-04-20 11:56:02 +0200

    feat: initialize tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
+import "styles/tailwind.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <div data-theme='night' className='min-h-screen w-full'>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </div>
   );
 }
 
