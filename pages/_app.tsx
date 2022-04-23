79659d674bcf4a96c89014086a145f93741d76c3
Abdallah Akrab <abdallahakrab98@gmail.com>
2022-04-24 01:46:02 +0200

    feat: use ApolloProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "styles/tailwind.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div data-theme='night' className='min-h-screen w-full'>
+    <div data-theme="night" className="min-h-screen w-full">
       <UserProvider>
-        <Component {...pageProps} />
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
       </UserProvider>
     </div>
   );
