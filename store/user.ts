0931ca083ebbff68b747e8c159c944899a986f52
Ali Raslan <software.development.14@olympicchannel.com>
2022-04-24 00:31:03 +0200

    feat: add persistent login state

--- store/user.ts
+++ store/user.ts
@@ -1,18 +1,21 @@
 import create from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 const useUserStore = create(
   devtools(
-    immer((set) => ({
-      user: {},
-      setUserValue: (value: Record<string, any>) => {
-        set((draft) => {
-          Object.assign(draft, value);
-        });
-      },
-    })),
-    { name: "user" }
+    persist(
+      immer((set) => ({
+        user: {},
+        setUserValue: (value: Record<string, any>) => {
+          set((draft) => {
+            Object.assign(draft, value);
+          });
+        },
+      })),
+      { name: "muchfund-user", getStorage: () => localStorage }
+    ),
+    { name: "muchfund-user-store" }
   )
 );
 
