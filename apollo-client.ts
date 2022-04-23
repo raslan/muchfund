73e816fb10ceaf638b96cfc66004a8238b44bfc8
Abdallah Akrab <abdallahakrab98@gmail.com>
2022-04-24 01:44:12 +0200

    feat: setup ApolloClient

---
+++ apollo-client.ts
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  // Countries API for testing
+  uri: "https://countries.trevorblades.com",
+  cache: new InMemoryCache(),
+});
+export default client;
