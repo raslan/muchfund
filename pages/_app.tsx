0199f5af4a1824f34730fc9cd404af595a6f0941
raslan <24810123+raslan@users.noreply.github.com>
2022-04-24 05:25:23 +0200

    feat: add theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,23 @@ import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div data-theme="night" className="min-h-screen w-full">
+    <ThemeProvider
+      attribute='data-theme'
+      defaultTheme='dark'
+      value={{ light: "winter", dark: "night" }}
+    >
       <UserProvider>
         <ApolloProvider client={client}>
-          <Component {...pageProps} />
+          <div className='min-h-screen w-full'>
+            <Component {...pageProps} />
+          </div>
         </ApolloProvider>
       </UserProvider>
-    </div>
+    </ThemeProvider>
   );
 }
 
