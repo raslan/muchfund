08bc86a5e93da81b1f207df3627abb11b89b5eca
Abdallah Akrab <abdallahakrab98@gmail.com>
2022-04-28 01:12:25 +0200

    feat: add utility getCurrency

--- lib/dinero.ts
+++ lib/dinero.ts
@@ -2,3 +2,6 @@ import { Currency } from "dinero.js";
 import * as AllCurrencies from "@dinero.js/currencies";
 
 export type CurrencyCode = keyof typeof AllCurrencies;
+
+export const getCurrency = (code: CurrencyCode): Currency<number> =>
+  AllCurrencies[code];
