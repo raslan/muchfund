964186ac737d352aa5ab127d6f44fe5e2d9154ff
raslan <24810123+raslan@users.noreply.github.com>
2022-05-01 05:37:20 +0200

    feat: create modal component

---
+++ components/atomic/Modal.tsx
@@ -0,0 +1,70 @@
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment } from 'react';
+
+const Modal = ({ isOpen, onClose, children, className }: Props) => {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as='div' className={`${className} relative`} onClose={onClose}>
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-black bg-opacity-75' />
+          </Transition.Child>
+
+          <div className='fixed inset-0 overflow-y-auto z-10'>
+            <div className='flex min-h-screen h-full items-center justify-center text-center'>
+              <Transition.Child
+                as={Fragment}
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+              >
+                <Dialog.Panel className='w-full max-w-max transform text-left align-middle transition-all lg:fixed lg:inset-y-auto'>
+                  <button onClick={onClose}>
+                    <svg
+                      className='text-base-content absolute top-10 right-5 w-7 h-7'
+                      viewBox='0 0 24 24'
+                      fill='none'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path
+                        d='M16.3956 7.75734C16.7862 8.14786 16.7862 8.78103 16.3956 9.17155L13.4142 12.153L16.0896 14.8284C16.4802 15.2189 16.4802 15.8521 16.0896 16.2426C15.6991 16.6331 15.0659 16.6331 14.6754 16.2426L12 13.5672L9.32458 16.2426C8.93405 16.6331 8.30089 16.6331 7.91036 16.2426C7.51984 15.8521 7.51984 15.2189 7.91036 14.8284L10.5858 12.153L7.60436 9.17155C7.21383 8.78103 7.21383 8.14786 7.60436 7.75734C7.99488 7.36681 8.62805 7.36681 9.01857 7.75734L12 10.7388L14.9814 7.75734C15.372 7.36681 16.0051 7.36681 16.3956 7.75734Z'
+                        fill='currentColor'
+                      />
+                      <path
+                        fillRule='evenodd'
+                        clipRule='evenodd'
+                        d='M4 1C2.34315 1 1 2.34315 1 4V20C1 21.6569 2.34315 23 4 23H20C21.6569 23 23 21.6569 23 20V4C23 2.34315 21.6569 1 20 1H4ZM20 3H4C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21H20C20.5523 21 21 20.5523 21 20V4C21 3.44772 20.5523 3 20 3Z'
+                        fill='currentColor'
+                      />
+                    </svg>
+                  </button>
+                  {children}
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+  className?: string;
+};
+
+export default Modal;
