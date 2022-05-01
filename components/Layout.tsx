b356d7c48db9cb77c3825c942284315792fb1de7
raslan <24810123+raslan@users.noreply.github.com>
2022-05-01 05:35:31 +0200

    feat: add meta tags to layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,19 @@
-import { useTheme } from "next-themes";
+import { useTheme } from 'next-themes';
+import Head from 'next/head';
 
 const Layout = ({ ...props }) => {
   const { theme } = useTheme();
   return (
-    <div className={`${theme === "dark" ? "dark " : ""}min-h-screen w-full`}>
-      {props.children}
-    </div>
+    <>
+      <Head>
+        <title>muchfund</title>
+        <meta name='description' content='A budgeting app for individuals' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className={`${theme === 'dark' ? 'dark' : ''} min-h-screen w-full`}>
+        {props.children}
+      </main>
+    </>
   );
 };
 
