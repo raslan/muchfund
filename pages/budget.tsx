39032462d2c8b8bcfba153aeea172e1588910670
raslan <24810123+raslan@users.noreply.github.com>
2022-05-01 05:35:12 +0200

    feat: create new budget interface

--- pages/budget.tsx
+++ pages/budget.tsx
@@ -1,168 +1,206 @@
+import * as AllCurrencies from '@dinero.js/currencies';
+import Modal from 'components/atomic/Modal';
+import Table from 'components/atomic/Table';
+import Form from 'components/Budget/Form';
+import DashboardStats from 'components/Dashboard/DashboardStats';
 import Navbar from 'components/Navbar';
 import Sidebar from 'components/Sidebar';
-import Form from 'components/Budget/Form';
+import {
+  add,
+  convert,
+  dinero,
+  Dinero,
+  down,
+  Rates,
+  subtract,
+  toSnapshot,
+  toUnit,
+} from 'dinero.js';
+import useCurrencyRates from 'hooks/useCurrencyRates';
+import { intlFormat } from 'lib/dinero';
 import { NextPage } from 'next';
-import Head from 'next/head';
-import { useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import useBudgetStore from 'store/budget';
+import { CashFlow } from 'types/finance';
 import { useIsClient } from 'usehooks-ts';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { dinero, toFormat, subtract, add, Currency, Dinero } from 'dinero.js';
-import * as AllCurrencies from '@dinero.js/currencies';
-import { CurrencyCode, getCurrency } from 'lib/dinero';
-
-// TODO: Refactor to separate components
-
-const price = dinero({ amount: 100, currency: AllCurrencies.USD });
-const transformer = ({
-  amount,
-  currency,
-}: {
-  amount: number;
-  currency: Currency<number>;
-}) => `${currency.code} ${amount}`;
-
-const incomeStreams = [
-  {
-    name: 'Salary',
-    amount: dinero({ amount: 100000, currency: AllCurrencies.USD }),
-  },
-  {
-    name: 'Youtube Channel',
-    amount: dinero({ amount: 25000, currency: AllCurrencies.USD }),
-  },
-];
-
-const expensesList = [
-  {
-    name: 'Rent',
-    amount: dinero({ amount: 12000, currency: AllCurrencies.USD }),
-  },
-  {
-    name: 'Food',
-    amount: dinero({ amount: 5000, currency: AllCurrencies.USD }),
-  },
-];
-const initialBudget = 1080 * 100;
 
 const Budget: NextPage = () => {
-  type Inputs = {
-    name: string;
-    amount: number;
-    currency: CurrencyCode;
-  };
-  const [income, setIncome] = useState(incomeStreams);
-  const [expenses, setExpenses] = useState(expensesList);
-  const [showIncome, setShowIncome] = useState(true);
-  const [showExpenses, setShowExpenses] = useState(true);
-  const [totalBudget, setTotalBudget] = useState(
-    dinero({ amount: initialBudget, currency: AllCurrencies.USD })
+  const { rates, refresh } = useCurrencyRates();
+  const [income, setIncome] = useState<CashFlow[]>([]);
+  const [expenses, setExpenses] = useState<CashFlow[]>([]);
+  const [showIncome, setShowIncome] = useState(false);
+  const [showExpenses, setShowExpenses] = useState(false);
+  const setBudgetStoreValue = useBudgetStore(
+    (state) => state.setBudgetStoreValue
   );
+  const [totalBudget, setTotalBudget] = useState<Dinero<number>>(
+    dinero({ amount: 0, currency: AllCurrencies.USD })
+  );
+  const [totalIncome, setTotalIncome] = useState<Dinero<number>[]>([
+    dinero({ amount: 0, currency: AllCurrencies.USD }),
+  ]);
+  const [totalExpenses, setTotalExpenses] = useState<Dinero<number>[]>([
+    dinero({ amount: 0, currency: AllCurrencies.USD }),
+  ]);
   const isClient = useIsClient();
-  // TODO: Doesn't work on different currencies
-  // need to check out dinero's:
-  //  * converters. link: https://v2.dinerojs.com/docs/api/conversions/convert
-  //  * custom calculators.
-  // need to figure out how we will manage fetching of rates periodically
-  // will we fetch rates each session or just fetch it periodically and store it in database get rates for currencies on demand
-  // should we separate expenses and income based on currency?
-  const addExpense = (amount: Dinero<number>) => {
-    const newTotalBudget = subtract(totalBudget, amount);
-    setTotalBudget(newTotalBudget);
-  };
-  const addIncome = (amount: Dinero<number>) => {
-    const newTotalBudget = add(totalBudget, amount);
-    setTotalBudget(newTotalBudget);
-  };
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
 
-  // TODO: Use https://headlessui.dev/react/combobox
-  const currencies = Object.entries(AllCurrencies).map(([key, value]) => (
-    <option key={key} value={key}>
-      {key}
-    </option>
-  ));
+  const getConvertedAmount = useCallback(
+    async (amount: Dinero<number>) => {
+      const currencyCode = toSnapshot(amount).currency.code;
+      const isUSD = currencyCode !== 'USD';
+      let tempRates;
+      if (!rates?.[currencyCode] && isUSD) {
+        tempRates = await refresh(currencyCode);
+      }
+      const convertedAmount = isUSD
+        ? convert(
+            amount,
+            AllCurrencies.USD,
+            (rates?.[currencyCode] || tempRates) as Rates<number>
+          )
+        : amount;
+      return convertedAmount;
+    },
+    [rates, refresh]
+  );
+
+  const addExpense = async (amount: Dinero<number>) => {
+    const amountInUSD = await getConvertedAmount(amount);
+    const newTotalBudget = subtract(totalBudget, amountInUSD);
+    setTotalExpenses([...totalExpenses, amountInUSD]);
+    setTotalBudget(newTotalBudget);
+  };
+
+  const addIncome = async (amount: Dinero<number>) => {
+    const amountInUSD = await getConvertedAmount(amount);
+    const newTotalBudget = add(totalBudget, amountInUSD);
+    setTotalIncome([...totalIncome, amountInUSD]);
+    setTotalBudget(newTotalBudget);
+  };
+
+  const totalIncomeMemo = useMemo(
+    () =>
+      totalIncome.reduce(
+        (acc, amount) => add(acc, amount),
+        dinero({ amount: 0, currency: AllCurrencies.USD })
+      ),
+    [totalIncome]
+  );
+
+  const totalExpensesMemo = useMemo(
+    () =>
+      totalExpenses.reduce(
+        (acc, amount) => add(acc, amount),
+        dinero({ amount: 0, currency: AllCurrencies.USD })
+      ),
+    [totalExpenses]
+  );
+
+  useEffect(() => {
+    const newTotalBudget = subtract(totalIncomeMemo, totalExpensesMemo);
+    setTotalBudget(newTotalBudget);
+    setBudgetStoreValue({
+      totalBudget: intlFormat(newTotalBudget, 'en-US'),
+      totalIncome: intlFormat(totalIncomeMemo, 'en-US'),
+      totalExpenses: intlFormat(totalExpensesMemo, 'en-US'),
+    });
+  }, [totalIncomeMemo, totalExpensesMemo, setBudgetStoreValue]);
 
   return (
     <div>
-      <Head>
-        <title>muchfund</title>
-        <meta name='description' content='A budgeting app for individuals' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <main className='h-full w-full flex flex-col justify-center px-3'>
+      <div className='h-full w-full flex flex-col justify-center'>
         {isClient && (
           <>
             <Navbar />
             <Sidebar>
-              <h1 className='text-6xl mb-10'>Budget</h1>
-              <div className='text-5xl flex mb-8'>
-                <h2 className=' '>Income</h2>
+              <h1 className='text-6xl mb-10 font-semibold'>Budget</h1>
+              <div className='mb-12'>
+                <DashboardStats />
+              </div>
+              <div className='text-5xl grid grid-cols-2 items-center max-w-max mb-8'>
+                <h2>Income</h2>
                 <button
-                  className='inline max-w-fit'
+                  className='btn btn-outline border-primary-focus rounded-sm hover:bg-primary hover:text-primary-content w-max ml-auto lg:ml-4 text-2xl leading-none font-light'
                   onClick={() => {
-                    setShowIncome(!showIncome);
+                    setShowIncome(true);
                   }}
                 >
-                  {showIncome ? '-' : '+'}
+                  +
                 </button>
               </div>
 
-              {/* TODO: Move to Modal */}
               {showIncome && (
-                <Form
-                  list={income}
-                  setList={setIncome}
-                  budgetMutate={addIncome}
-                ></Form>
-              )}
-
-              <ul className='mb-8'>
-                {income.map((item) => (
-                  <li key={item.name} className='my-4'>
-                    {item.name} {toFormat(item.amount, transformer)}
-                  </li>
-                ))}
-              </ul>
-              <div className='text-5xl flex mb-8'>
-                <h2 className=' '>Expenses</h2>
-                <button
-                  className='inline max-w-fit'
-                  onClick={() => {
-                    setShowExpenses(!showExpenses);
+                <Modal
+                  isOpen={showIncome}
+                  onClose={() => {
+                    setShowIncome(false);
                   }}
                 >
-                  {showExpenses ? '-' : '+'}
+                  <Form
+                    title='Add income'
+                    className='border shadow-sm shadow-base-content'
+                    list={income}
+                    setList={setIncome}
+                    budgetMutate={addIncome}
+                  />
+                </Modal>
+              )}
+
+              <div className='mb-8'>
+                <Table
+                  tableData={income.map((item) => ({
+                    name: item.name,
+                    amount: toUnit(item.amount, {
+                      digits: 2,
+                      round: down,
+                    }),
+                    currency: toSnapshot(item.amount).currency.code,
+                  }))}
+                />
+              </div>
+              <div className='text-5xl grid grid-cols-2 items-center max-w-max mb-8'>
+                <h2>Expenses</h2>
+                <button
+                  className='btn btn-outline border-primary-focus rounded-sm hover:bg-primary hover:text-primary-content w-max ml-auto lg:ml-4 text-2xl leading-none font-light'
+                  onClick={() => {
+                    setShowExpenses(true);
+                  }}
+                >
+                  +
                 </button>
               </div>
-              {/* TODO: Move to Modal */}
               {showExpenses && (
-                <Form
-                  list={expenses}
-                  setList={setExpenses}
-                  budgetMutate={addExpense}
-                ></Form>
+                <Modal
+                  isOpen={showExpenses}
+                  onClose={() => {
+                    setShowExpenses(false);
+                  }}
+                >
+                  <Form
+                    title='Add expense'
+                    className='border shadow-sm shadow-base-content'
+                    list={expenses}
+                    setList={setExpenses}
+                    budgetMutate={addExpense}
+                  />
+                </Modal>
               )}
-              <ul className='mb-10'>
-                {expenses.map((item) => (
-                  <li key={item.name} className='mb-4'>
-                    {item.name} {toFormat(item.amount, transformer)}
-                  </li>
-                ))}
-              </ul>
-              <div>
-                <h2 className='text-5xl flex mb-8'>Total Budget</h2>
-                <h3 className='text-2xl'>
-                  {toFormat(totalBudget, transformer)}
-                </h3>
+              <div className='mb-10'>
+                <Table
+                  tableData={expenses.map((item) => ({
+                    name: item.name,
+                    amount: toUnit(item.amount, {
+                      digits: 2,
+                      round: down,
+                    }),
+                    currency: toSnapshot(item.amount).currency.code,
+                  }))}
+                />
               </div>
             </Sidebar>
           </>
         )}
-      </main>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,28 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import React from "react";
-import Sidebar from "components/Sidebar";
-import DashboardStats from "components/Dashboard/DashboardStats";
-import Navbar from "components/Navbar";
-import { useIsClient } from "usehooks-ts";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import React, { useState } from 'react';
+import Sidebar from 'components/Sidebar';
+import DashboardStats from 'components/Dashboard/DashboardStats';
+import Navbar from 'components/Navbar';
+import { useIsClient } from 'usehooks-ts';
+import Loading from 'components/atomic/Loading';
 
 const Home: NextPage = () => {
   const isClient = useIsClient();
-
   return (
     <div>
-      <Head>
-        <title>muchfund</title>
-        <meta name='description' content='A budgeting app for individuals' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <main className='h-full w-full flex flex-col justify-center px-3'>
-        {!isClient && (
-          <div className='flex flex-col items-center justify-center h-screen p-0 m-0'>
-            <svg
-              className='animate-spin w-16 h-16'
-              viewBox='0 0 24 24'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                opacity='0.2'
-                fillRule='evenodd'
-                clipRule='evenodd'
-                d='M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z'
-                fill='currentColor'
-              />
-              <path
-                d='M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z'
-                fill='currentColor'
-              />
-            </svg>
-          </div>
-        )}
+      <div className='h-full w-full flex flex-col justify-center'>
+        {!isClient && <Loading isFullScreen />}
         {isClient && (
           <>
             <Navbar />
             <Sidebar>
+              <h1 className='text-6xl mb-10 font-semibold'>Summary</h1>
               <DashboardStats />
             </Sidebar>
           </>
         )}
-      </main>
+      </div>
     </div>
   );
 };
