e719defe4ece2a84c1417936860e78c44cd3ecd4
raslan <24810123+raslan@users.noreply.github.com>
2022-05-01 05:36:14 +0200

    style: format with prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
-import { ApolloProvider } from "@apollo/client";
-import { UserProvider } from "@auth0/nextjs-auth0";
-import Layout from "components/Layout";
-import client from "lib/apollo";
-import { ThemeProvider } from "next-themes";
-import type { AppProps } from "next/app";
-import "styles/tailwind.css";
+import { ApolloProvider } from '@apollo/client';
+import { UserProvider } from '@auth0/nextjs-auth0';
+import Layout from 'components/Layout';
+import client from 'lib/apollo';
+import { ThemeProvider } from 'next-themes';
+import type { AppProps } from 'next/app';
+import 'styles/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider
       attribute='data-theme'
       defaultTheme='dark'
-      value={{ light: "winter", dark: "night" }}
+      value={{ light: 'winter', dark: 'night' }}
     >
       <UserProvider>
         <ApolloProvider client={client}>
