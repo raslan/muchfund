ed842ed826a9226a0ab3f3e0cd49ab410f8ce846
raslan <24810123+raslan@users.noreply.github.com>
2022-05-01 05:33:46 +0200

    feat: create intl formatter

--- lib/dinero.ts
+++ lib/dinero.ts
@@ -1,7 +1,29 @@
-import { Currency } from "dinero.js";
-import * as AllCurrencies from "@dinero.js/currencies";
+import { Currency, Dinero, toFormat } from 'dinero.js';
+import * as AllCurrencies from '@dinero.js/currencies';
 
 export type CurrencyCode = keyof typeof AllCurrencies;
 
 export const getCurrency = (code: CurrencyCode): Currency<number> =>
   AllCurrencies[code];
+
+export const intlFormat = (
+  dineroObject: Dinero<number>,
+  locale: string,
+  options = {}
+) => {
+  const transformer = ({
+    amount,
+    currency,
+  }: {
+    amount: number;
+    currency: Currency<number>;
+  }) => {
+    return amount.toLocaleString(locale, {
+      ...options,
+      style: 'currency',
+      currency: currency.code,
+    });
+  };
+
+  return toFormat(dineroObject, transformer);
+};
