3acc57d71772212e1528641f7a21b4c472288b79
raslan <24810123+raslan@users.noreply.github.com>
2022-05-02 19:51:28 +0200

    refactor: rewrite budget page to use primitives

--- pages/budget.tsx
+++ pages/budget.tsx
@@ -1,68 +1,41 @@
-import * as AllCurrencies from '@dinero.js/currencies';
 import Modal from 'components/atomic/Modal';
 import Table from 'components/atomic/Table';
 import Form from 'components/budget/Form';
 import DashboardStats from 'components/dashboard/DashboardStats';
 import Navbar from 'components/Navbar';
 import Sidebar from 'components/Sidebar';
+import { add, Dinero, subtract } from 'dinero.js';
+import useCurrency from 'hooks/useCurrency';
 import {
-  add,
-  convert,
-  dinero,
-  Dinero,
-  down,
-  Rates,
-  subtract,
-  toSnapshot,
-  toUnit,
-} from 'dinero.js';
-import useCurrencyRates from 'hooks/useCurrencyRates';
-import { intlFormat } from 'lib/dinero';
+  calculateDineroSum,
+  createEmptyDinero,
+  getCurrencyCode,
+  getNumberFromDinero,
+  intlFormat,
+} from 'lib/dinero';
 import { NextPage } from 'next';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import useBudgetStore from 'store/budget';
 import { CashFlow } from 'types/finance';
-import { useIsClient } from 'usehooks-ts';
 
 const Budget: NextPage = () => {
-  const { rates, refresh } = useCurrencyRates();
+  const { getConvertedAmount } = useCurrency();
+  const setBudgetStoreValue = useBudgetStore(
+    (state) => state.setBudgetStoreValue
+  );
   const [income, setIncome] = useState<CashFlow[]>([]);
   const [expenses, setExpenses] = useState<CashFlow[]>([]);
   const [showIncome, setShowIncome] = useState(false);
   const [showExpenses, setShowExpenses] = useState(false);
-  const setBudgetStoreValue = useBudgetStore(
-    (state) => state.setBudgetStoreValue
-  );
   const [totalBudget, setTotalBudget] = useState<Dinero<number>>(
-    dinero({ amount: 0, currency: AllCurrencies.USD })
+    createEmptyDinero()
   );
   const [totalIncome, setTotalIncome] = useState<Dinero<number>[]>([
-    dinero({ amount: 0, currency: AllCurrencies.USD }),
+    createEmptyDinero(),
   ]);
   const [totalExpenses, setTotalExpenses] = useState<Dinero<number>[]>([
-    dinero({ amount: 0, currency: AllCurrencies.USD }),
+    createEmptyDinero(),
   ]);
-  const isClient = useIsClient();
-
-  const getConvertedAmount = useCallback(
-    async (amount: Dinero<number>) => {
-      const currencyCode = toSnapshot(amount).currency.code;
-      const isUSD = currencyCode !== 'USD';
-      let tempRates;
-      if (!rates?.[currencyCode] && isUSD) {
-        tempRates = await refresh(currencyCode);
-      }
-      const convertedAmount = isUSD
-        ? convert(
-            amount,
-            AllCurrencies.USD,
-            (rates?.[currencyCode] || tempRates) as Rates<number>
-          )
-        : amount;
-      return convertedAmount;
-    },
-    [rates, refresh]
-  );
 
   const addExpense = async (amount: Dinero<number>) => {
     const amountInUSD = await getConvertedAmount(amount);
@@ -78,21 +51,15 @@ const Budget: NextPage = () => {
     setTotalBudget(newTotalBudget);
   };
 
+  // Use memoization to calculate total income because it's a heavy operation
   const totalIncomeMemo = useMemo(
-    () =>
-      totalIncome.reduce(
-        (acc, amount) => add(acc, amount),
-        dinero({ amount: 0, currency: AllCurrencies.USD })
-      ),
+    () => calculateDineroSum(totalIncome),
     [totalIncome]
   );
 
+  // Use memoization for expenses as well because of the same operation
   const totalExpensesMemo = useMemo(
-    () =>
-      totalExpenses.reduce(
-        (acc, amount) => add(acc, amount),
-        dinero({ amount: 0, currency: AllCurrencies.USD })
-      ),
+    () => calculateDineroSum(totalExpenses),
     [totalExpenses]
   );
 
@@ -100,106 +67,98 @@ const Budget: NextPage = () => {
     const newTotalBudget = subtract(totalIncomeMemo, totalExpensesMemo);
     setTotalBudget(newTotalBudget);
     setBudgetStoreValue({
-      totalBudget: intlFormat(newTotalBudget, 'en-US'),
-      totalIncome: intlFormat(totalIncomeMemo, 'en-US'),
-      totalExpenses: intlFormat(totalExpensesMemo, 'en-US'),
+      totalBudget: intlFormat(newTotalBudget),
+      totalIncome: intlFormat(totalIncomeMemo),
+      totalExpenses: intlFormat(totalExpensesMemo),
     });
   }, [totalIncomeMemo, totalExpensesMemo, setBudgetStoreValue]);
 
   return (
     <div>
       <div className='h-full w-full flex flex-col justify-center'>
-        {isClient && (
-          <>
-            <Navbar />
-            <Sidebar>
-              <h1 className='text-6xl mb-10 font-semibold'>Budget</h1>
-              <div className='mb-12'>
-                <DashboardStats />
-              </div>
-              <div className='text-5xl grid grid-cols-2 items-center max-w-max mb-8'>
-                <h2>Income</h2>
-                <button
-                  className='btn btn-outline border-primary-focus rounded-sm hover:bg-primary hover:text-primary-content w-max ml-auto lg:ml-4 text-2xl leading-none font-light'
-                  onClick={() => {
-                    setShowIncome(true);
-                  }}
-                >
-                  +
-                </button>
-              </div>
+        <>
+          <Navbar />
+          <Sidebar>
+            <h1 className='text-6xl mb-10 font-semibold'>Budget</h1>
+            <div className='mb-12'>
+              <DashboardStats />
+            </div>
+            <div className='text-5xl grid grid-cols-2 items-center max-w-max mb-8'>
+              <h2>Income</h2>
+              <button
+                className='btn btn-outline border-primary-focus rounded-sm hover:bg-primary hover:text-primary-content w-max ml-auto lg:ml-4 text-2xl leading-none font-light'
+                onClick={() => {
+                  setShowIncome(true);
+                }}
+              >
+                +
+              </button>
+            </div>
 
-              {showIncome && (
-                <Modal
-                  isOpen={showIncome}
-                  onClose={() => {
-                    setShowIncome(false);
-                  }}
-                >
-                  <Form
-                    title='Add income'
-                    className='border shadow-sm shadow-base-content'
-                    list={income}
-                    setList={setIncome}
-                    budgetMutate={addIncome}
-                  />
-                </Modal>
-              )}
+            {showIncome && (
+              <Modal
+                isOpen={showIncome}
+                onClose={() => {
+                  setShowIncome(false);
+                }}
+              >
+                <Form
+                  title='Add income'
+                  className='border shadow-sm shadow-base-content'
+                  list={income}
+                  setList={setIncome}
+                  budgetMutate={addIncome}
+                />
+              </Modal>
+            )}
 
-              <div className='mb-8'>
-                <Table
-                  tableData={income.map((item) => ({
-                    name: item.name,
-                    amount: toUnit(item.amount, {
-                      digits: 2,
-                      round: down,
-                    }),
-                    currency: toSnapshot(item.amount).currency.code,
-                  }))}
+            <div className='mb-8'>
+              <Table
+                tableData={income.map((item) => ({
+                  name: item.name,
+                  amount: getNumberFromDinero(item.amount),
+                  currency: getCurrencyCode(item.amount),
+                }))}
+              />
+            </div>
+            <div className='text-5xl grid grid-cols-2 items-center max-w-max mb-8'>
+              <h2>Expenses</h2>
+              <button
+                className='btn btn-outline border-primary-focus rounded-sm hover:bg-primary hover:text-primary-content w-max ml-auto lg:ml-4 text-2xl leading-none font-light'
+                onClick={() => {
+                  setShowExpenses(true);
+                }}
+              >
+                +
+              </button>
+            </div>
+            {showExpenses && (
+              <Modal
+                isOpen={showExpenses}
+                onClose={() => {
+                  setShowExpenses(false);
+                }}
+              >
+                <Form
+                  title='Add expense'
+                  className='border shadow-sm shadow-base-content'
+                  list={expenses}
+                  setList={setExpenses}
+                  budgetMutate={addExpense}
                 />
-              </div>
-              <div className='text-5xl grid grid-cols-2 items-center max-w-max mb-8'>
-                <h2>Expenses</h2>
-                <button
-                  className='btn btn-outline border-primary-focus rounded-sm hover:bg-primary hover:text-primary-content w-max ml-auto lg:ml-4 text-2xl leading-none font-light'
-                  onClick={() => {
-                    setShowExpenses(true);
-                  }}
-                >
-                  +
-                </button>
-              </div>
-              {showExpenses && (
-                <Modal
-                  isOpen={showExpenses}
-                  onClose={() => {
-                    setShowExpenses(false);
-                  }}
-                >
-                  <Form
-                    title='Add expense'
-                    className='border shadow-sm shadow-base-content'
-                    list={expenses}
-                    setList={setExpenses}
-                    budgetMutate={addExpense}
-                  />
-                </Modal>
-              )}
-              <div className='mb-10'>
-                <Table
-                  tableData={expenses.map((item) => ({
-                    name: item.name,
-                    amount: toUnit(item.amount, {
-                      digits: 2,
-                      round: down,
-                    }),
-                    currency: toSnapshot(item.amount).currency.code,
-                  }))}
-                />
-              </div>
-            </Sidebar>
-          </>
-        )}
+              </Modal>
+            )}
+            <div className='mb-10'>
+              <Table
+                tableData={expenses.map((item) => ({
+                  name: item.name,
+                  amount: getNumberFromDinero(item.amount),
+                  currency: getCurrencyCode(item.amount),
+                }))}
+              />
+            </div>
+          </Sidebar>
+        </>
       </div>
     </div>
   );
