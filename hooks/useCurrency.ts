89f9dee1b722019b6c5284f64ecf49ba5fdeb11d
raslan <24810123+raslan@users.noreply.github.com>
2022-05-02 19:50:53 +0200

    feat: rename useCurrency hook and add conversion logic

--- hooks/useCurrency.ts
+++ hooks/useCurrency.ts
@@ -1,20 +1,26 @@
 import axios from 'axios';
+import { convert, Dinero, Rates, toSnapshot } from 'dinero.js';
+import { Currencies } from 'lib/dinero';
 import { useCallback, useEffect, useState } from 'react';
 import useCurrencyStore from 'store/rates';
 
-const useCurrencyRates = () => {
+const useCurrency = () => {
   const rates = useCurrencyStore((state) => state.rates);
   const setCurrencyStoreValue = useCurrencyStore(
     (state) => state.setCurrencyStoreValue
   );
   const [error, setError] = useState<any>(null);
 
+  /**
+   * @param currency The currency code
+   * @returns Exchange rates for that currency, and stores them in the "rates" object in the currency store
+   */
   const refresh = useCallback(
     async (currency: string = 'USD') => {
       setError(null);
       try {
         const response = await axios.get(
-          `https://api.coinbase.com/v2/exchange-rates?currency=${currency}`
+          `${process.env.NEXT_PUBLIC_CURRENCY_API}?currency=${currency}`
         );
         const formattedRates = Object.entries(response.data.data.rates)
           .map(([key, value]: any) => {
@@ -50,13 +56,37 @@ const useCurrencyRates = () => {
     [setCurrencyStoreValue, rates]
   );
 
+  /**
+   * @param {Dinero<number>} dineroObject
+   * @returns {Dinero<number>} converted to the base currency
+   */
+  const getConvertedAmount = useCallback(
+    async (amount: Dinero<number>) => {
+      const currencyCode = toSnapshot(amount).currency.code;
+      const isUSD = currencyCode !== 'USD';
+      let tempRates;
+      if (!rates?.[currencyCode] && isUSD) {
+        tempRates = await refresh(currencyCode);
+      }
+      const convertedAmount = isUSD
+        ? convert(
+            amount,
+            Currencies.USD,
+            (rates?.[currencyCode] || tempRates) as Rates<number>
+          )
+        : amount;
+      return convertedAmount;
+    },
+    [rates, refresh]
+  );
+
   useEffect(() => {
     if (!rates) {
       refresh();
     }
   }, [rates, refresh]);
 
-  return { rates, error, refresh };
+  return { rates, error, refresh, getConvertedAmount };
 };
 
-export default useCurrencyRates;
+export default useCurrency;
