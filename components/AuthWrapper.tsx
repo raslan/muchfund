6d6eb753f4fbc05894cb92d2149d2e7d61958a35
raslan <24810123+raslan@users.noreply.github.com>
2022-05-06 22:55:35 +0200

    fix: refactor object render on auth wrapper

--- components/AuthWrapper.tsx
+++ components/AuthWrapper.tsx
@@ -1,17 +1,20 @@
 import { useUser } from '@auth0/nextjs-auth0';
 import { isDev } from 'lib/utils';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Loading from './atomic/Loading';
 
 const AuthWrapper = ({ children }: Props): React.ReactElement => {
   const router = useRouter();
   const { user, isLoading } = useUser();
-  if (isLoading) return <Loading isFullScreen />;
-  if (!user && !isDev()) {
-    return <>{router.push('/api/auth/login', undefined, { shallow: true })}</>;
-  }
-  return <>{children}</>;
+
+  useEffect(() => {
+    if (!isLoading && !user && !isDev())
+      router.push('/', undefined, { shallow: true });
+  }, [isLoading, user, router]);
+
+  if ((isLoading || !user) && !isDev()) return <Loading isFullScreen />;
+  else return <>{children}</>;
 };
 
 type Props = {
