9bc1855d04121ba2912072969952544b84d2f4c2
raslan <24810123+raslan@users.noreply.github.com>
2023-02-13 09:16:05 +0200

    refactor: cleanup app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,12 @@
-import { ApolloProvider } from '@apollo/client';
-import { UserProvider } from '@auth0/nextjs-auth0';
-import Layout from 'components/Layout';
-import client from 'lib/apollo';
-import { ThemeProvider } from 'next-themes';
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import 'styles/tailwind.css';
-import 'regenerator-runtime';
+import { Inter } from '@next/font/google';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const inter = Inter({ subsets: ['latin'] });
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute='data-theme'
-      defaultTheme='dark'
-      value={{ light: 'winter', dark: 'night' }}
-    >
-      <UserProvider>
-        <ApolloProvider client={client}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ApolloProvider>
-      </UserProvider>
-    </ThemeProvider>
+    <div className={`${inter.className}`}>
+      <Component {...pageProps} />
+    </div>
   );
 }
-
-export default MyApp;
