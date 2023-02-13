0d305f4eb14352cb85514a2317b29efb7e6018af
raslan <24810123+raslan@users.noreply.github.com>
2023-02-13 09:15:46 +0200

    chore: remove apollo

--- lib/apollo.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  // Countries API for testing
-  uri: 'https://countries.trevorblades.com',
-  cache: new InMemoryCache(),
-});
-export default client;
