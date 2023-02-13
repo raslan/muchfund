63125cf28e92c901f4b7ac85b96e7c6a59fe526a
raslan <24810123+raslan@users.noreply.github.com>
2023-02-13 09:17:06 +0200

    refactor: cleanup homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,18 @@
-import { useUser } from '@auth0/nextjs-auth0';
-import Loading from 'components/atomic/Loading';
-import FeaturesSection from 'components/home/Features';
-import HeroSection from 'components/home/Hero';
-import Navbar from 'components/Navbar';
-import { isDev } from 'lib/utils';
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import Head from 'next/head';
 
-const Home: NextPage = () => {
-  const router = useRouter();
-  const { user, isLoading } = useUser();
-
-  useEffect(() => {
-    if (!isLoading && user && !isDev())
-      router.push('/dashboard', undefined, { shallow: true });
-  }, [isLoading, user, router]);
-
-  if ((isLoading || user) && !isDev()) return <Loading isFullScreen />;
-  else
-    return (
-      <div>
-        <div className='h-full w-full max-w-screen-2xl m-auto flex flex-col justify-center bg-pattern'>
-          <>
-            <Navbar />
-            <HeroSection />
-            <FeaturesSection />
-          </>
-        </div>
-      </div>
-    );
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>muchfund</title>
+        <meta
+          name='description'
+          content='A multi-currency budget calculator focused on speed and productivity.'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main></main>
+    </>
+  );
+}
